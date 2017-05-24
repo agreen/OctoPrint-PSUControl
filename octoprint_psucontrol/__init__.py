1ed025f249e21cc441956e08890879fb816f9b2b
Shawn Bruce <kantlivelong@gmail.com>
2017-05-24 13:14:05 -0400

    Return 0/1 for getPSUState API command instead of T/F

--- octoprint_psucontrol/__init__.py
+++ octoprint_psucontrol/__init__.py
@@ -385,7 +385,7 @@ class PSUControl(octoprint.plugin.StartupPlugin,
             else:
                 self.turn_psu_on()
         elif command == 'getPSUState':
-            return jsonify(isPSUOn=self.isPSUon)
+            return jsonify(isPSUOn=int(self.isPSUon))
 
     def get_settings_defaults(self):
         return dict(
